7fb6d392eee8595e6d317154108e8cbbc293b405
Zhenxun Du <duzhenxun@xin.com>
2019-06-04 16:19:01 +0800

    git

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => '127.0.0.1',
+    'hostname'       => '10.10.10.31',
     // 数据库名
     'database'       => 'tp5admin',
     // 用户名
